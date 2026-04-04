d3cf923bc15bf4205d5bde65a335d8e44ec3784d
AlejandroAballe <alejandroaballe20@gmail.com>
2026-04-04 18:55:06 +0200

    Añadir relaciones en modelos Instrument y Course

--- backend/app/Models/Course.php
+++ backend/app/Models/Course.php
@@ -4,8 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Course extends Model
 {
     use HasFactory;
+
+    public function courses(): BelongsTo
+    {
+        return $this->belongsTo(Instrument::class);
+    }
+
+    public function users(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class);
+    }
 }
--- backend/app/Models/Instrument.php
+++ backend/app/Models/Instrument.php
@@ -4,8 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Instrument extends Model
 {
     use HasFactory;
+
+    public function courses(): HasMany
+    {
+        return $this->hasMany(Course::class);
+    }
+
+    public function users(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class)->withPivot('level');
+    }
 }
