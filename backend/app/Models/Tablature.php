db22876faf16fa7c161f2a5076aba398730a4329
AlejandroAballe <alejandroaballe20@gmail.com>
2026-04-04 18:57:02 +0200

    Añadir relaciones a modelos Song y Tablature

--- backend/app/Models/Tablature.php
+++ backend/app/Models/Tablature.php
@@ -4,8 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Tablature extends Model
 {
     use HasFactory;
+
+    public function song(): BelongsTo
+    {
+        return $this->belongsTo(Song::class);
+    }
+
+    public function instrument(): BelongsTo
+    {
+        return $this->belongsTo(Instrument::class);
+    }
 }
