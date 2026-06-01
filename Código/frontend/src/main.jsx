34e7c03617889a6aeec2bd72955d1b7679e5c66a
AlejandroAballe <alejandroaballe20@gmail.com>
2026-06-01 17:23:21 +0200

    parche para css

--- Código/frontend/src/main.jsx
+++ Código/frontend/src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
+import './index.css';
 import { AuthProvider } from './context/AuthContext';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
